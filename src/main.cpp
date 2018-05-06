174cffa818b46e065763b8a47b5ebb72e235010d
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2018-05-06 13:20:22 +0100

    Print out code/feedback for each guess

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
+#include <iostream>
+#include <sstream>
+#include <algorithm>
 #include "mastermind.h"
 #include "autosolve.h"
-
-#include <iostream>
 
 int main() {
     auto secret = GenerateSecret();
     auto initialContext = AutosolveContext();
     const auto guesses = Autosolve(secret, initialContext);
     std::cout << "Number of guesses: " << guesses.size() << std::endl;
+    std::for_each(
+            guesses.cbegin(),
+            guesses.cend(), [](const std::pair<Code, Feedback> &pair) {
+                const auto code = pair.first;
+                const auto feedback = pair.second;
+                std::stringstream sscode;
+                std::stringstream ssfeedback;
+                sscode << code.pegs()[0] << code.pegs()[1] << code.pegs()[2] << code.pegs()[3];
+                ssfeedback << feedback.blacks() << feedback.whites();
+                std::cout
+                        << "guess: " << sscode.str()
+                        << " "
+                        << "feedback: " << ssfeedback.str()
+                        << std::endl;
+            });
 }
